5b48d4f6faf9a7e640953f7864f51d16904013ad
funkyhippo <funkyhippo@f-ck.me>
2022-05-31 23:40:48 -0400

    Fix totalMem -> totalmem.

--- src/server.ts
+++ src/server.ts
@@ -67,7 +67,7 @@ function validateEnvironmentVariables() {
 
 function memWatchdog() {
   const freeMem = os.freemem();
-  const totalMem = os.totalMem();
+  const totalMem = os.totalmem();
   console.log(`Memory usage: ${freeMem / totalMem * 100}%, free: ${freeMem / 1024 / 1024}MB, total: ${totalMem / 1024 / 1024}MB`);
   setTimeout(memWatchdog, 5000);
 }
